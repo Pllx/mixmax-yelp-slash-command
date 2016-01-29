6351fb7f9ef2d5b9e7f20aa117ad255b57926228
Wai Hwa Tan <twaih92@gmail.com>
2016-01-28 22:51:56 -0800

    cleaned test for resolver

--- test/yelp-slash-command-test.js
+++ test/yelp-slash-command-test.js
@@ -16,48 +16,28 @@ describe('Typeahead tests', function() {
 
 describe('Resolver tests', function() {
   var invalidJSON = {
-    name : 'Tom',
-    species : 'cat'
+    name : 'Mr. Meeseeks',
+    species : 'Meeseeks'
   };
+
+  // trimmed for relevant fields
   var validJSON = {
     text: `{
-            "is_claimed":true,
-            "rating":4.5,
-            "mobile_url":"http://m.yelp.com/biz/the-codmother-fish-and-chips-san-francisco?utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=Vfk_xJ5DrQBJBKxY3v5VsQ",
             "rating_img_url":"http://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png",
             "review_count":1679,
             "name":"The Codmother Fish and Chips",
-            "rating_img_url_small":"http://s3-media2.fl.yelpcdn.com/assets/2/www/img/a5221e66bc70/ico/stars/v1/stars_small_4_half.png",
             "url":"http://www.yelp.com/biz/the-codmother-fish-and-chips-san-francisco?utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=Vfk_xJ5DrQBJBKxY3v5VsQ",
             "categories":[["British","british"],["Fish & Chips","fishnchips"],["Seafood","seafood"]],
-            "phone":"4156069349",
-            "snippet_text":"finger lickin good\\n\\norder takes a while to make\\n\\nhave patience young one",
             "image_url":"http://s3-media4.fl.yelpcdn.com/bphoto/7PytB3z6QbM22DFpW8mK8Q/ms.jpg",
-            "snippet_image_url":"http://s3-media3.fl.yelpcdn.com/photo/4XbiVpEIL2cR4VHBtjyXbw/ms.jpg",
-            "display_phone":"+1-415-606-9349",
-            "rating_img_url_large":"http://s3-media4.fl.yelpcdn.com/assets/2/www/img/9f83790ff7f6/ico/stars/v1/stars_large_4_half.png",
-            "id":"the-codmother-fish-and-chips-san-francisco",
-            "is_closed":false,
             "location":
               {
-                "cross_streets":"Jones St & Taylor St",
-                "city":"San Francisco",
-                "display_address":["496 Beach  St","North Beach/Telegraph Hill","San Francisco, CA 94133"],
-                "geo_accuracy":9.5,
-                "neighborhoods":["North Beach/Telegraph Hill","Fisherman\'s Wharf"],
-                "postal_code":"94133",
-                "country_code":"US",
-                "address":["496 Beach  St"],
-                "coordinate":{
-                  "latitude":37.8071157,
-                  "longitude":-122.4172602
-                },
-                "state_code":"CA"
+                "display_address":["496 Beach  St","North Beach/Telegraph Hill","San Francisco, CA 94133"]
               }
-            }`,
-            user: 'example@gmail.com'
-          };
-  var listing = JSON.parse(validJSON.text.trim())
+          }`,
+    user: 'example@gmail.com'
+  };
+
+  var listing = JSON.parse(validJSON.text.trim());
 
   it('Empty requests should return status code 400', function(done) {
     request(app)
